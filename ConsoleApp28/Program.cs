9bc01385234512cbcc23ba2b25b9dacdc8d9b031
NHOY <NHOY@2210DR000919>
2023-05-19 02:57:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInLine;
+            int durationDoctorAppointment = 10;
+            int minutesInHour = 60;
+
+            Console.WriteLine("Вы пришли в поликлинику и видите очередь из людей. Так как времени у вас не много, вы" +
+                " можете расчитать: сколько времени у вас уйдёт на ожидание свой очереди.");
+            Console.Write("Сколько людей в очереди:");
+            peopleInLine = Convert.ToInt32(Console.ReadLine());
+
+            int result = peopleInLine * durationDoctorAppointment;
+            int waitingTimeHours = result / minutesInHour;
+            int waitingTimeMinutes = result % minutesInHour;
+
+            Console.WriteLine("Время ожидания в очереди - " + waitingTimeHours + " часов и " + waitingTimeMinutes + " минут.");
+        }
+    }
+}
